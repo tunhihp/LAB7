87d5adfc50c08d02b767c7a6e60670602cfcccd5
tunhihp <125249969+tunhihp@users.noreply.github.com>
2023-03-27 15:53:48 +0700

    Add files via upload

---
+++ LAB 7.java
@@ -0,0 +1,27 @@
+abstract class XeMay {
+  public abstract void brake();
+}
+
+class XeMayTheThao extends XeMay {
+  @Override
+  public void brake() {
+    System.out.println("Xe máy thể thao đang phanh bằng hệ thống phanh đĩa");
+  }
+}
+
+class XeMayLeoNui extends XeMay {
+  @Override
+  public void brake() {
+    System.out.println("Xe máy leo núi đang phanh bằng hệ thống phanh cơ");
+  }
+}
+
+public class Main {
+  public static void main(String[] args) {
+    XeMay theThao = new XeMayTheThao();
+    theThao.brake(); // output: Xe máy thể thao đang phanh bằng hệ thống phanh đĩa
+
+    XeMay leoNui = new XeMayLeoNui();
+    leoNui.brake(); // output: Xe máy leo núi đang phanh bằng hệ thống phanh cơ
+  }
+}
